02113cfa7a4b0eadd9f5e1bd1f6ebbfb361affda
ArvinZaker <arv.zaker@gmail.com>
2023-05-15 00:44:37 -0400

    updated resume

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pandoc Resume.md --pdf-engine=xelatex -o "Arvin Zaker - Resume.pdf"
-pandoc Resume.md -p plain -o resume.txt
+pandoc Resume.md -t plain -o ./resume.txt
 # pandoc Resume.md -o "Arvin Zaker - Resume.html" --standalone
 # pandoc Resume.md -o Resume.docx
